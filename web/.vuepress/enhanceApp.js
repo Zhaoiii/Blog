4fdb4434bed3fe92a296fe8b274e0cf3880e8db7
Zhaoiii <zdhzdhzdhzdhzdh@gmail.com>
2022-03-24 21:05:44 +0800

    feat: deploy

--- web/.vuepress/enhanceApp.js
+++ web/.vuepress/enhanceApp.js
@@ -1,12 +1,11 @@
 export default ({ router }) => {
-    router.beforeEach((to, from, next) => {
-        // console.log(_hmt)
-        // if (typeof _hmt !== "undefined") {
-        //     if (to.path) {
-        //         _hmt.push(["_trackPageview", to.fullPath]);
-        //     }
-        // }
-
-        next();
-    });
+    // router.beforeEach((to, from, next) => {
+    //     console.log(_hmt, 1)
+    //     if (typeof _hmt !== "undefined") {
+    //         if (to.path) {
+    //             _hmt.push(["_trackPageview", to.fullPath]);
+    //         }
+    //     }
+    //     next();
+    // });
 };
